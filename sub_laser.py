1870683226e544f2f32939efd7ac8b8db76e9663
JunewooHong <110160075+JuneWooHong@users.noreply.github.com>
2022-11-24 15:51:09 +0900

    Update sub_laser.py

--- sub_laser.py
+++ sub_laser.py
@@ -23,41 +23,39 @@ class SubLaser(Node):
         self.scan = LaserScan()
         self.move_tb3 = MoveTB3()
         
-        #self.laser_cam_pub = self.create_publisher(String, 'laser_cam', 10)
-        
+        self.laser_cam_pub = self.create_publisher(String, 'laser_cam', 10)
         
     def get_scan(self, msg):
         self.scan = msg
-        #print("front = %s" %(self.scan.ranges[0]))
-        #print("left  = %s" %(self.scan.ranges[90]))
-        #print("back  = %s" %(self.scan.ranges[180]))
-        #print("right = %s" %(self.scan.ranges[270]))
-        #print("--------------------------")
+        print("front = %s" %(self.scan.ranges[0]))
+        print("left  = %s" %(self.scan.ranges[90]))
+        print("back  = %s" %(self.scan.ranges[180]))
+        print("right = %s" %(self.scan.ranges[270]))
+        print("--------------------------")
         
-        
-        #a = radians(float(input("input rotation (deg): ")))
-        #d = float(input("input distance (m)  : "))
-        #node.rotate(a)
-        #node.straight(d)
-        
-        #self.move_tb3.straight(0.5)
-        #self.enermy = 1
-        #self.angle_stack = 0
         for i in range(0, 360):
-        #    print("angle[%d] : %s" %(i, self.scan.ranges[i]))
-            if (float(self.scan.ranges[i]) > 0.0 and float(self.scan.ranges[i]) < 0.2):
-        #        msg = String()
-        #        msg.data = 'find'
-        #        self.laser_cam_pub.publish(msg)
-                
-                self.move_tb3.rotate(radians(i))
-        #        self.move_tb3.straight(-0.2)
-        #    	self.angle_stack += i
-        #    	self.enermy += 1
-
-        #if 30 < self.enermy <= 50:
-        #	print(self.angle_stack // self.enermy, "도 방향 미상물체 접근중!!!!!!!")
-        #	self.move_tb3.rotate(radians(self.angle_stack // self.enermy))
+            if (0.0 < float(self.scan.ranges[i]) < 0.35):
+            	msg = String()
+            	msg.data = 'find'
+            	self.laser_cam_pub.publish(msg)
+        
+            	print(i, "도 방향 미상물체 !!!!!!!")
+            	if (i > 180):
+            		self.move_tb3.rotate(radians(-(360-i)))
+            		self.move_tb3.straight(-0.03)
+            		msg.data = 'end_find'
+            		self.laser_cam_pub.publish(msg)
+            		break
+            	else:
+            		self.move_tb3.rotate(radians(i))
+            		self.move_tb3.straight(-0.03)
+            		msg.data = 'end_find'
+            		self.laser_cam_pub.publish(msg)
+            		break
+            	
+            	#msg = String()
+            	
+        
         
 def main(args=None):
     rclpy.init(args=args)
