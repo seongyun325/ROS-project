9602a13783db6afe0ab38fa2a721b5ed10291d1d
JinseokIs <112450334+JinseokIs@users.noreply.github.com>
2022-11-24 11:35:12 +0900

    Update sub_laser.py

--- sub_laser.py
+++ sub_laser.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import LaserScan
 from ar_track.move_tb3 import MoveTB3
 from math import radians, degrees, sqrt, atan2
 
+from std_msgs.msg import String
+
 class SubLaser(Node):
     
     def __init__(self):
@@ -21,27 +23,41 @@ class SubLaser(Node):
         self.scan = LaserScan()
         self.move_tb3 = MoveTB3()
         
+        #self.laser_cam_pub = self.create_publisher(String, 'laser_cam', 10)
+        
         
     def get_scan(self, msg):
         self.scan = msg
-        print("front = %s" %(self.scan.ranges[0]))
-        print("left  = %s" %(self.scan.ranges[90]))
-        print("back  = %s" %(self.scan.ranges[180]))
-        print("right = %s" %(self.scan.ranges[270]))
-        print("--------------------------")
+        #print("front = %s" %(self.scan.ranges[0]))
+        #print("left  = %s" %(self.scan.ranges[90]))
+        #print("back  = %s" %(self.scan.ranges[180]))
+        #print("right = %s" %(self.scan.ranges[270]))
+        #print("--------------------------")
+        
+        
         #a = radians(float(input("input rotation (deg): ")))
         #d = float(input("input distance (m)  : "))
         #node.rotate(a)
         #node.straight(d)
         
         #self.move_tb3.straight(0.5)
-        
+        #self.enermy = 1
+        #self.angle_stack = 0
         for i in range(0, 360):
-            if (float(self.scan.ranges[i]) > 0.2 and float(self.scan.ranges[i]) < 0.3):
-            	print(i, "도 방향 미상물체 접근중!!!!!!!")
-            	self.move_tb3.rotate(radians(i))
-            	break
-        
+        #    print("angle[%d] : %s" %(i, self.scan.ranges[i]))
+            if (float(self.scan.ranges[i]) > 0.0 and float(self.scan.ranges[i]) < 0.2):
+        #        msg = String()
+        #        msg.data = 'find'
+        #        self.laser_cam_pub.publish(msg)
+                
+                self.move_tb3.rotate(radians(i))
+        #        self.move_tb3.straight(-0.2)
+        #    	self.angle_stack += i
+        #    	self.enermy += 1
+
+        #if 30 < self.enermy <= 50:
+        #	print(self.angle_stack // self.enermy, "도 방향 미상물체 접근중!!!!!!!")
+        #	self.move_tb3.rotate(radians(self.angle_stack // self.enermy))
         
 def main(args=None):
     rclpy.init(args=args)
@@ -60,4 +76,3 @@ def main(args=None):
             
 if __name__ == '__main__':
     main()
-
