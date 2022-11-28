8169d2d8e2f400d540bab959ea553c74fd1612e6
JunewooHong <110160075+JuneWooHong@users.noreply.github.com>
2022-11-28 10:06:30 +0900

    Update follow_waypoints.py

--- follow_waypoints.py
+++ follow_waypoints.py
@@ -1,20 +1,87 @@
-import rclpy
-import time
+import rclpy, time
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped 
 from rclpy.action import ActionClient
 from action_msgs.msg import GoalStatus
 from nav2_msgs.action import FollowWaypoints
 # from rclpy.duration import Duration # Handles time for ROS 2
+from rclpy.exceptions import ParameterNotDeclaredException
+from rcl_interfaces.msg import ParameterType
 
-#goal = 0
+from std_msgs.msg import String
+
+go_stop = 'go'
 
 class ClientFollowPoints(Node):
 
     def __init__(self):
         super().__init__('client_follow_points')
         self._client = ActionClient(self, FollowWaypoints, '/FollowWaypoints')
+        #self.declare_parameter('mode', 'move')
+        self.go_stop_sub = self.create_subscription (
+        	String,
+        	'waypoint_control',
+        	self.go_stop_callback,
+        	10)
 
+        self.declare_parameter('waypoints', 'point1')
+        timer_period = 1	# seconds
+        self.timer = self.create_timer(timer_period, self.send_waypoints)
+        self.tw = Twist()
+        self.rgoal = PoseStamped()
+        
+    
+    def go_stop_callback(self, data):
+    	global go_stop
+    	go_stop = data.data
+        
+    def send_waypoints(self):
+    	global go_stop
+    	param = self.get_parameter('waypoints').get_parameter_value().string_value
+    	
+    	if go_stop == 'go':
+    		if param == 'point1':
+    			#print("11111111")
+    			self.rgoal.header.frame_id = "map"
+    			self.rgoal.header.stamp.sec = 0
+    			self.rgoal.header.stamp.nanosec = 0
+    			
+    			self.rgoal.pose.position.z = -0.00143
+    			self.rgoal.pose.position.x = 0.661
+    			self.rgoal.pose.position.y = 0.157
+    			
+    			self.rgoal.pose.orientation.w = 1.0
+    		elif param == 'point2':
+    			#print("22222222")
+    			self.rgoal.header.frame_id = "map"
+    			self.rgoal.header.stamp.sec = 0
+    			self.rgoal.header.stamp.nanosec = 0
+    			
+    			self.rgoal.pose.position.z = -0.00143
+    			self.rgoal.pose.position.x = 1.03
+    			self.rgoal.pose.position.y = -0.345
+    			
+    			self.rgoal.pose.orientation.w = 1.0
+    		elif param == 'point3':
+    			#print("33333333")
+    			self.rgoal.header.frame_id = "map"
+    			self.rgoal.header.stamp.sec = 0
+    			self.rgoal.header.stamp.nanosec = 0
+    			
+    			self.rgoal.pose.position.z = -0.00143
+    			self.rgoal.pose.position.x = 1.91
+    			self.rgoal.pose.position.y = -0.195
+    			
+    			self.rgoal.pose.orientation.w = 1.0
+    			
+    		self.get_logger().info('%s' % param)
+    		mgoal = [self.rgoal]
+    		self.send_points(mgoal)
+    	else:
+    		self.tw.linear.x = 0.0
+    		self.tw.angular.z = 0.0
+    		
     def send_points(self, points):
         msg = FollowWaypoints.Goal()
         msg.poses = points
@@ -33,8 +100,6 @@ class ClientFollowPoints(Node):
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
-        #if goal == 1: goal = 0
-        #elif goal == 0: goal = 1
 
     def get_result_callback(self, future):
         result = future.result().result
@@ -48,29 +113,9 @@ def main(args=None):
     rclpy.init(args=args)
 
     follow_points_client = ClientFollowPoints()
-    print('client inited')
     
-    rgoal = PoseStamped()
-    rgoal.header.frame_id = "map"
-    rgoal.header.stamp.sec = 0
-    rgoal.header.stamp.nanosec = 0
-    
-    while True:
-    	#if goal == 0:
-    		rgoal.pose.position.z = 0.0
-    		rgoal.pose.position.x = 0.185
-    		rgoal.pose.position.y = 0.202
-    		
-    	#elif goal == 1:
-    		rgoal.pose.position.z = 0.0
-    		rgoal.pose.position.x = 0.897
-    		rgoal.pose.position.y = 0.17
-    		
-    	rgoal.pose.orientation.w = 1.0
-    	mgoal = [rgoal]
-    	follow_points_client.send_points(mgoal)
-    	#rclpy.spin_once(follow_points_client)
-    	
+    rclpy.spin(follow_points_client)
+	
     	
 if __name__ == '__main__':
 	main()
